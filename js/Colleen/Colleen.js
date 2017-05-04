054dd80e6baf53ec30b508c7765de1f4d02d6737
Dorian ROLY <droly@e1r6p9.42.fr>
2017-05-04 14:05:54 +0200

    envoie tamerlaput

---
+++ js/Colleen/Colleen.js
@@ -0,0 +1,9 @@
+function main() {var i = 0; /*
+yo
+*/}
+console.log(main.toString());
+var e = "console.log(main.tostring());\nvar e = \""+e+"\"\nconsole.log(e);\n/*\nyo\n*/";
+console.log(e);
+/*
+yo
+*/
